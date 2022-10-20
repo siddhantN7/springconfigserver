6861ea9a0b4fd81377c10d6171317969fe2c87ca
Siddhant <siddhant.ashutosh07@gmail.com>
2022-10-20 14:30:05 -0300

    Adding server

---
+++ config-server/src/main/java/sid/server/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package sid.server.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
---
+++ config-server/src/test/java/sid/server/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sid.server.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
